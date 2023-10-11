666e4eff5af200e6e7b3fdfad51d3aece550142e
Vishwas Yeleshwaram <145417535+yvishwas40@users.noreply.github.com>
2023-10-11 16:58:25 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,175 @@
+export const bio = [
+    "Hello there, This is Vishwas Yeleshwaram.",
+    `A web development intern, programmer- python programming language and a problem solver.`,
+    `Eager to apply my technical expertise and in-depth
+    product understanding to drive impactful contributions and exceed expectations in challenging projects.`,
+    "Thanks for taking the time to learn a little about me!",
+];
+
+export const skills = [
+    {
+        title: "Languages",
+        skillName: "Python, programming",
+        color: "1",
+        percentage: "80",
+    },
+    
+    {
+        title: "Design",
+        skillName: "HTML, CSS, JavaScript",
+        color: "4",
+        percentage: "60",
+    },
+    {
+        title: "Version Control",
+        skillName: "GitHub",
+        color: "7",
+        percentage: "70",
+    },
+    
+    
+    {
+        title: "IDE",
+        skillName: "VS Code",
+        color: "6",
+        percentage: "70",
+    },
+    {
+        title: "co-curicular",
+        skillName: "Public Speaking",
+        color: "3",
+        percentage: "80",
+    },
+];
+
+export const projects = {
+    webProjects: [
+        {
+            projectName: "Travel Website",
+            image: "https://colorlib.com/wp/wp-content/uploads/sites/2/travelo-free-template.jpg",
+            summary:
+                "Developed a basic Travel website preview with the concepts of HTML and CSS",
+            techStack: ["HTML","CSS"],
+        },
+        
+    ],
+    softwareProjects: [
+        {
+            projectName: "Prakruthi Ayurvedha ChatBot",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY9YSBzDu9VYu1GgQxFHUAuwyB-uN-SLhMgw&usqp=CAU",
+            summary:
+                "ChatBot that results the ayurvedic prakruthi type based on the survey",
+           
+            techStack: ["Flask","Jason"],
+        },
+        
+        
+    ],
+    androidProjects: [
+        
+    ],
+    freelanceProjects: [
+        
+    ],
+};
+
+export const experience = [
+    {
+        title: "Motion Cut",
+        duration: "october- 2023 - Novemeber-2023",
+        subtitle: "Intern",
+        details: [
+            "Intern in Web Development"
+        ],
+        tags: ["HTML","CSS","JAVA SCRIPT", "BootStrap"],
+        icon: "heartbeat",
+    },
+];
+
+export const education = [
+    {
+        title: "B.E(B-Tech). in Computer Science and Engineering",
+        duration: "2022 - 2026",
+        subtitle: "Chaitanya Bharati Institute Of Technology-Hyderabad",
+        details: [],
+        tags: [
+            "Python Programming",
+            "Operating Systems",
+            "Database Management System",
+            "Machine Learning",
+            "Artificial Intelligence",
+            "Cloud Computing",
+        ],
+        icon: "graduation-cap",
+    },
+    {
+        title: "Class 12th in Science and Mathematics",
+        duration: "2020-2022",
+        subtitle: "Sri chaitanya Junior College, Hyderabad",
+        details: [],
+        tags: ["Physics", "Chemistry", "Mathematics"],
+        icon: "book",
+    },
+];
+
+export const footer = [
+    {
+        label: "Dev Profiles",
+        data: [
+            
+            {
+                text: "GitHub",
+                link: "https://github.com/yvishwas40",
+            },
+            {
+                text: "CodeChef",
+                link: "https://www.codechef.com/users/yvishwas40",
+            },
+        ],
+    },
+    {
+        label: "Resources",
+        data: [
+            {
+                text: "Enable Dark/Light Mode",
+                func: "enableDarkMode()",
+            },
+            {
+                text: "Print this page",
+                func: "window.print()",
+            },
+            
+        ],
+    },
+    {
+        label: "Social Profiles",
+        data: [
+            {
+                text: "Linkedin",
+                link: "https://www.linkedin.com/in/vishwas-yeleshwaram-956a9b251",
+            },
+            {
+                text: "X",
+                link: "https://twitter.com/Vishwas_chary",
+            },
+            {
+                text: "Instagram",
+                link: "https://www.instagram.com/vishwas_yeleshwaram/",
+            },
+            
+        ],
+    },
+    {
+        label: "copyright-text",
+        data: [
+            "by Vishwas Yeleshwaram.",
+        ],
+    },
+];
+
+const gitUserName = "vinaysomawat";
+const mediumUserName = "vinaysomawat";
+
+export const URLs = {
+    mediumURL: `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${mediumUserName}`,
+};
